0df438570f03e7830720003591e6637748275e7d
jeffbaut13 <jeferssonabv@gmail.com>
2026-04-27 18:30:28 -0500

    home!

--- src/components/home/SectionThree.jsx
+++ src/components/home/SectionThree.jsx
@@ -2,13 +2,11 @@ export const SectionThree = () => {
   const data_section_three = [
     {
       id: 1,
-
       image: "/imagenes/section-three/antigua.webp",
       image_mobile: "/imagenes/section-three/antiguaM.webp",
     },
     {
       id: 2,
-
       image: "/imagenes/section-three/nuevo.webp",
       image_mobile: "/imagenes/section-three/nuevoM.webp",
     },
